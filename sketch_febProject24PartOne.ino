0318dfc346a927bf0eae7322ec88edf14ea91974
cahoward123 <caiden-howard@student.kirkwood.edu>
2026-02-05 10:03:33 -0600

    Add files via upload

---
+++ sketch_febProject24PartOne.ino
@@ -0,0 +1,32 @@
+// Project 25 – Using Interrupts
+
+#define LED 13
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(8,9,4,5,6,7);
+void setup()
+{
+  lcd.begin(16, 2);
+  pinMode(13, OUTPUT);
+  attachInterrupt(0, displayHappy, RISING);
+  attachInterrupt(1, displaySad, RISING);  
+}
+void displayHappy()
+{
+  lcd.clear();
+  lcd.print( ":)" );
+}
+
+void displaySad()
+{
+  lcd.clear();
+  lcd.print( ":(");
+}
+
+void loop()
+{
+  
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
